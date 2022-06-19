185c561a83d7dd7185b8eee927fb243faf4e945c
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-19 12:40:40 -0500

    task router fleshed out

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,16 @@
 // build your `/api/tasks` router here
+const router = require('express').Router()
+const Task = require('./model')
+
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
+})
+
+router.post('/', async (req,res) => {
+    const task = req.body
+    const newTask = await Resource(task)
+    res.json(newTask)
+})
+module.exports = router
